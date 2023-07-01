7f4fdfbf017ed13ffe8f1dd38fd0b3e49bc66687
Poshehon <vladislav.maksimov@math.msu.ru>
2023-07-02 02:07:36 +0300

    weee

--- abstraction.hs
+++ abstraction.hs
@@ -1,4 +1,54 @@
 {-
+class Functor f where
+    fmap :: (a -> b) -> f a -> f b
+
+instance Functor [] where
+    fmap = map
+
+instance Functor Maybe where
+    fmap f (Just a) = Just (f a)
+    fmap _ Nothing = Nothing
+
+instance Functor IO where
+    fmap f action = do
+        result <- action
+        return (f result)
+
+1. fmap id = id
+2. fmap (f . g) = fmap f . fmap g
+
+class (Functor f) => Applicative f where
+    pure :: a -> f a
+    (<*>) :: f (a -> b) -> f a -> f b
+
+instance Applicative Maybe where
+    pure x = Just x
+    Nothing <*> _ = Nothing
+    (Just f) <*> Nothing = Nothing
+    (Just f) <*> Just x = Just (f x)
+
+instance Applicative Maybe where
+    pure x = Just x
+    Nothing <*> _ = Nothing
+    (Just f) <*> x = fmap f x
+
+instance Applicative [] where
+    pure x = [x]
+    fs <*> xs = [f x | f <- sx, x <- xs]
+
+instance Applicative IO where
+    pure = return
+    a <*> b = do
+        f <- a
+        x <- b
+        return (f x)
+
+1. pure f <*> x = fmap f x
+2. pure id <*> v = v
+3. pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+4. pure f <*> pure x = pure (f x)
+5. u <*> pure y = pure ($ y) <*> u
+
 class Monoid a where
     mempty :: a
     mappend :: a -> a -> a
@@ -11,29 +61,21 @@ instance Monoid a => Monoid (Maybe a) where
     mappend x Nothing = x
     mappend (Just a) (Just b) = Just (a `mappend` b)
 
-class Functor f where
-    fmap :: (a -> b) -> f a -> f b
+1. mempty `mappend` x = x
+2. x `mappend` mempty = x
+3. (x `mappend` y) `mappend` z = x `mappend` (y `mappend` z)
 
-instance Functor [] where
-    fmap = map
+newtype SumInt = SumInt Integer deriving (Show, Num)
+newtype ProdInt = ProdInt Integer deriving (Show, Num)
 
-instance Functor ((,) s) where
-    fmap g (x,y) = (x, g y)
+instance Monoid SumInt where
+    mempty = 0
+    mappend (SumInt x) (SumInt y) = SumInt (x + y)
 
-class Monad m where
-    return :: a -> m a
-    (>>=) :: m a -> (a -> m b) -> m b
-    (>>) :: m a -> m b -> m b
-    x >> y = x >>= \_ -> y
+instance Monoid ProdInt where
+    mempty = 1
+    mappend (ProdInt x) (ProdInt y) = ProdInt (x*y)
 
-    fail :: String -> m a
-    fail s = error s
+class (Applicative m) => Monad m where
+    (>>=)  :: m a -> (a -> m b) -> m b
 -}
--- Это работало раньше, но теперь так нельзя
--- Надо сначала делать аппликативные функторы
-{-newtype Id a = Id {get :: a} deriving (Show)
-
-instance Monad Id where
-    return = Id
-    (>>=) (Id x) f = f x
-    -}
---
+++ monads.hs
@@ -0,0 +1,9 @@
+data Id a = Id a deriving (Show)
+
+instance Functor Id where
+    fmap f (Id a) = Id (f a)
+instance Applicative Id where
+    pure x = Id x
+    (Id f) <*> (Id x) = Id (f x)
+instance Monad Id where
+    (Id x) >>= f = f (x)
